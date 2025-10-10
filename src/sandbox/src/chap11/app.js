b18e7451492e47cbfd603e4bb6ba399b827142b7
Michel Kreutner <mkreutner@gmail.com>
2025-10-10 10:43:11 +0200

    chore: playing with DOM elements

--- src/sandbox/src/chap11/app.js
+++ src/sandbox/src/chap11/app.js
@@ -36,5 +36,33 @@ function alertUser(event) {
     : "Alert User";
 }
 
+function createLogin(event) {
+  const mainApp = document.querySelector("#mainApp");
+
+  const formLogin = document.createElement("div");
+  formLogin.setAttribute("id", "frmLogin");
+  formLogin.setAttribute("name", "frmLogin");
+  formLogin.classList.add("form");
+  const formGroupUsername = document.createElement("div");
+  formGroupUsername.classList.add("form-group");
+  const labelUsername = document.createElement("label");
+  labelUsername.setAttribute("id", "lblUsername");
+  labelUsername.setAttribute("name", "lblUsername");
+  labelUsername.setAttribute("for", "inUsername");
+  labelUsername.innerHTML = "Username:";
+  const inputUsername = document.createElement("input");
+  inputUsername.setAttribute("id", "inUsername");
+  inputUsername.setAttribute("name", "inUsername");
+  inputUsername.setAttribute("type", "text");
+
+  if (mainApp.querySelector("#frmLogin") === null) {
+    formGroupUsername.appendChild(labelUsername);
+    formGroupUsername.appendChild(inputUsername);
+    formLogin.appendChild(formGroupUsername);
+    mainApp.appendChild(formLogin);
+  }
+}
+
 document.querySelector("#bChgImg").addEventListener("click", changeImg);
 document.querySelector("#bAlertUser").addEventListener("click", alertUser);
+document.querySelector("#bLoginMe").addEventListener("click", createLogin);
