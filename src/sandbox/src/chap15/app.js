a1545c3ceb8c8d12657cb6897c65861b1c7e9b0b
Michel Kreutner <mkreutner@gmail.com>
2025-10-13 11:37:24 +0200

    chore: explore network - wip

--- src/sandbox/src/chap15/app.js
+++ src/sandbox/src/chap15/app.js
@@ -5,7 +5,11 @@ const baseUrl = "https://my-json-server.typicode.com/mkreutner/data-mock/";
 const runL109 = false; // Première requête HTTP avec fetch
 const runL110 = false; // Effectuer une requête POST
 const runL111 = false; // Les CORS
-const runL112 = true; // Options avancées des requêtes fetch
+const runL112 = false; // Options avancées des requêtes fetch
+const runL113 = false; // Annuler des requêtes en cours
+const runL114 = false; // Les objets FormData
+const runL115 = true; // La Web API URL
+
 //#region Première requête HTTP avec fetch
 if (runL109) {
   const fetchAllUsers = fetch(`${baseUrl}users`);
@@ -107,5 +111,78 @@ if (runL112) {
     .catch((e) => {
       console.error(e);
     });
+}
+//#endregion
+
+//#region Annuler des requêtes en cours
+if (runL113) {
+  // Create AbortController
+  const controleur = new AbortController();
+
+  setTimeout(() => controleur.abort(), 11);
+
+  try {
+    const fetchUsers = fetch(`${baseUrl}users`, {
+      signal: controleur.signal,
+      cache: "no-cache",
+    });
+    fetchUsers.then(async (res) => {
+      try {
+        const users = await res.json();
+        console.log(users);
+      } catch (e) {
+        if (e.name === "AbortError") {
+          console.error("Resquest has been canceled!");
+        }
+      }
+    });
+  } catch (err) {
+    if (err.name === "AbortError") {
+      console.error("Resquest has been canceled!");
+    }
+  }
+}
+//#endregion
+
+//#region Les objets FormData
+if (runL114) {
+  const form = document.querySelector("#formCreateUser");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const firstname = formData.get("firstname").toLocaleLowerCase();
+    const lastname = formData.get("lastname").toLocaleLowerCase();
+    formData.append("username", firstname[0] + lastname);
+
+    const newUser = {
+      first_name: "John",
+      last_name: "Doe",
+      username: "jdoe",
+    };
+    const options = {
+      method: "POST",
+      boby: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    const createUser = fetch(`${baseUrl}users`, options);
+    createUser.then(async (response) => {
+      try {
+        const users = await response.json();
+        console.log(users);
+      } catch (e) {
+        console.error(e);
+      }
+    });
+  });
+}
+//#endregion
+
+//#region La Web API URL
+if (runL115) {
 }
 //#endregion
