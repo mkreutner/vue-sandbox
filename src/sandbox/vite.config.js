59b521ddca5279d778707b4a6a8e3917b45edf7a
Michel Kreutner <mkreutner@gmail.com>
2025-10-13 16:28:39 +0200

    chore(project): 02 - create and config

--- src/sandbox/vite.config.js
+++ src/sandbox/vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [],
   build: {
     outDir: "./dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/project02/index.html"),
+        form: resolve(__dirname, "src/project02/form/form.html"),
+      },
+    },
   },
   // root: "./exercises/08/06",
-  root: "./",
+  root: "./src/project02/",
+  // root: "./",
   server: {
     host: "0.0.0.0",
     port: 8080,
